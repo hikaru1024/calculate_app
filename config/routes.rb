e9a1259557f41dc4c546748a28f37519f2409229
shoko1228 <shoko.in.kenya@gmail.com>
2023-06-21 20:40:05 +0900

    ルーティング微修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'payments' => 'payments#index'
   get 'payments/new' => 'payments#new'
   get 'payments/:id/edit' => 'payments#edit'
-  get 'payments/calculates'  => 'payments#calculates'
+  get 'payments/:event_id/calculates'  => 'payments#calculates'
 
   get 'events/new'
   root 'pages#index'
