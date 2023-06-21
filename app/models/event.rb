c65e914880f9c3e70d981ae635c93e0b99d82018
shoko1228 <shoko.in.kenya@gmail.com>
2023-06-21 17:06:33 +0900

    has_many 追記

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+    has_many :members, dependent: :destroy
+    has_many :payments, dependent: :destroy
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,4 @@
 class Member < ApplicationRecord
+    has_many :payments, dependent: :destroy
+    has_many :payment_details, dependent: :destroy
 end
