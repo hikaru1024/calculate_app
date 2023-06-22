bb47e297387adc89f9e64bbf24354fb3b7696e02
fasfafal <nogulin0530@gmail.com>
2023-06-21 19:34:17 -0700

    l

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,16 @@
 class MembersController < ApplicationController
+    def index
+    end
+    def show
+    end
+    def new
+        @member = Member.new
+    end
+    def create
+        @member = member.create(member_params)
+    end
+    private
+     def member_params
+        params.require(:member).permit(:name)
+     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root 'events#new'
+  post 'events/create'
+  get '/:event_code', to: 'events#index'
   get 'payments' => 'payments#index'
   get 'payments/new' => 'payments#new'
   get 'payments/:id/edit' => 'payments#edit'
   get 'payments/:event_id/calculates'  => 'payments#calculates'
 
-  get 'events/new'
-  root 'pages#index'
+  resources :members
+  
 end
